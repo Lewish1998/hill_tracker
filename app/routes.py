678f8734ea9e97784c892f587a15710e71bb5dbd
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2024-05-08 23:03:36 +0100

    forms & styling

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
 from app.models import User
@@ -13,8 +14,34 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+        
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    return render_template('signup.html')
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Account created. Please log in")
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
--- config.py
+++ config.py
@@ -7,3 +7,5 @@ load_dotenv()
 class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    DEBUG = True
+    TESTING = True
