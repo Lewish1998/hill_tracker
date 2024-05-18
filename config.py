8de4ab8cd3907ff8c9fc78e4cb7916ef0cfbbea8
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2024-05-18 22:22:06 +0100

    clean up

--- config.py
+++ config.py
@@ -6,11 +6,10 @@ load_dotenv()
 
 username=os.getenv('USERNAME')
 password=os.getenv('PASSWORD')
+pgpass=os.getenv('PG_PASS')
 
 class Config():
     SECRET_KEY = os.getenv('SECRET_KEY') # or 'dev'
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     # SQLALCHEMY_DATABASE_URI = f"postgresql://{username}:{password}@localhost/hill_tracker"
-    SQLALCHEMY_DATABASE_URI = f"postgresql://book_tracker_owner:a4cyjfOdUlT9@ep-orange-waterfall-a2vxog0q.eu-central-1.aws.neon.tech/book_tracker?sslmode=require"
-    DEBUG = True
-    TESTING = True
+    SQLALCHEMY_DATABASE_URI = f"postgresql://book_tracker_owner:{pgpass}.eu-central-1.aws.neon.tech/book_tracker?sslmode=require"
