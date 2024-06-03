eebd67a153ba802af0ec65786749f1a47df521bf
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2024-06-03 14:08:45 +0100

    test

--- config.py
+++ config.py
@@ -9,7 +9,5 @@ password=os.getenv('PASSWORD')
 pgpass=os.getenv('PG_PASS')
 
 class Config():
-    SECRET_KEY = os.getenv('SECRET_KEY') # or 'dev'
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    # SQLALCHEMY_DATABASE_URI = f"postgresql://{username}:{password}@localhost/hill_tracker"
-    SQLALCHEMY_DATABASE_URI = f"postgresql://book_tracker_owner:{pgpass}.eu-central-1.aws.neon.tech/book_tracker?sslmode=require"
+    SECRET_KEY = os.getenv('SECRET_KEY') or 'dev'
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'app.db')}"
--- run.py
+++ run.py
@@ -1,3 +1,1 @@
 from app import app
-
-#  gunicorn --workers=2 --log-level=info app:app
